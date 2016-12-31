ec506fcaa7d72eff81ff2b246d1fe28b89a1f0aa
Kenji Fujiwara <niyaton@gmail.com>
2016-12-31 14:01:00 +0900

    test version

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ echo "installing fisherman..."
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 echo "installing theme..."
-fish -c 'fisher install omf/theme-scorphish'
+cat init-fish | fish -i
