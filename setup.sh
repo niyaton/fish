8d96a33df938234f6886eef3f1054b44913dd784
Kenji Fujiwara <niyaton@gmail.com>
2016-12-31 23:39:25 +0900

    change repository name to fish-config

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ echo "installing fisherman..."
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 echo "installing all-my settings..."
-fish -i -c "fisher install niyaton/fish"
+fish -i -c "fisher install niyaton/fish-config"
