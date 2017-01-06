4ca3834923b073afb0c5d564e01de402974156ce
Kenji Fujiwara <niyaton@gmail.com>
2017-01-06 21:10:12 +0900

    initial commit for windows

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ echo "installing fisherman..."
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 
 echo "installing all-my settings..."
-fish -i -c "fisher install niyaton/fish-config"
+fish -i -c "fisher install niyaton/fish-config@windows"
