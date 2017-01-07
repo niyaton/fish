a4e5cf61232e2674fdebb450a942d7fa17d23c1a
Kenji Fujiwara <niyaton@gmail.com>
2017-01-08 03:36:46 +0900

    remove default vi mode prompting

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,17 +1,3 @@
 function fish_mode_prompt --description 'Displays the current mode'
-    # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        switch $fish_bind_mode
-            case default
-                echo '[N]'
-            case insert
-                echo '[I]'
-            case replace-one
-                echo '[R]'
-            case visual
-                echo '[V]'
-        end
-        set_color normal
-        echo -n ' '
-    end
+    # Do nothing because niyaton/theme-scorphish prompts vi mode
 end
